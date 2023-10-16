830452f380898612ea1bd2fb4e9256ee9f8712b3
selemani <selemanilusu@gmail>
2023-10-16 14:33:16 +0200

    Add Dependency Injection to Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using bus_registration.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BusRegistrationDbContext>(Options=>
+Options.UseSqlServer(builder.Configuration.GetConnectionString("BusRegistrationConnectionString")));
 
 var app = builder.Build();
 
